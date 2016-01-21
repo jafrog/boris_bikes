448e52c0f8b000ad11a99a42f4cc15526edb31bc
jafrog <howeveririna@gmail.com>
2016-01-21 10:32:42 +0000

    Ch 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,15 +8,24 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if bikes.empty?
+    fail 'No bikes available' if empty?
     bikes.pop
   end
 
   def dock(bike)
-    fail 'Capacity limit reached' if bikes.count >= 20
+    fail 'Capacity limit reached' if full?
     bikes << bike
   end
 
   private
+
   attr_writer :bikes
+
+  def full?
+    bikes.count >= 20
+  end
+
+  def empty?
+    bikes.empty?
+  end
 end
